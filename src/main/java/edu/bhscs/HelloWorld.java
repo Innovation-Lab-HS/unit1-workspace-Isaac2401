98f32ac7b13d315082e9bfc5dcd1ae010c31bb93
Isaac O <96074139+Isaac2401@users.noreply.github.com>
2024-09-13 18:20:13 +0000

    message thing (idk)

---
+++ src/main/java/edu/bhscs/HelloWorld.java
@@ -0,0 +1,9 @@
+class HelloWorld {
+  // all code in java sits inside a class
+
+  // every java program needs a public main method
+  public static void main(String[] args) {
+      // for the next while, ALL code you write will live inside main()
+    System.out.println("Hello, World!");
+  }
+}
